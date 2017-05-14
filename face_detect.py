cd78b3b712cdb2a626cbb73bddc7b849bdb8caf9
= <acgourley@gmail.com>
2017-05-14 01:13:01 +0000

    working face detect to gpio

--- face_detect.py
+++ face_detect.py
@@ -1,47 +1,76 @@
-from picamera.array import PiRGBArray
-from picamera import PiCamera
 import time
 import cv2
 import sys
 import imutils
+import RPi.GPIO as GPIO
+
+unwatched = GPIO.LOW
+watched = GPIO.HIGH
+
+if len(sys.argv) > 1:
+  unwatched = GPIO.HIGH
+  watched = GPIO.LOW
+
+GPIO.setmode(GPIO.BOARD)
+iopin = 40
+
+GPIO.setup(iopin, GPIO.OUT)
+GPIO.output(iopin, unwatched)
 
 # Create the haar cascade
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 # initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-camera.resolution = (160, 120)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(160, 120))
-
+cam = cv2.VideoCapture(0)
+cam.set(3, 160)
+cam.set(4, 120)
 # allow the camera to warmup
 time.sleep(0.1)
-lastTime = time.time()*1000.0
-# capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-  # grab the raw NumPy array representing the image, then initialize the timestamp
-  # and occupied/unoccupied text
-  image = frame.array
+
+lastWatched = -10
+lastOutput = unwatched
+
+while True:
+  ret_val, image = cam.read()
+  image = cv2.flip(image, 1)
+  thisTime = time.time()
   gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
   # Detect faces in the image
-  faces = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
-  print(time.time()*1000.0-lastTime," Found {0} faces!".format(len(faces)))
-  lastTime = time.time()*1000.0
+  faces = faceCascade.detectMultiScale(
+  gray,
+  scaleFactor=1.1,
+  minNeighbors=5,
+  minSize=(30, 30),
+  flags = cv2.CASCADE_SCALE_IMAGE
+  )
+  #print(time.time()*1000.0-lastTime," Found {0} faces!".format(len(faces)))
+  
   # Draw a rectangle around the faces
   for (x, y, w, h) in faces:
-    cv2.circle(image, (x+w/2, y+h/2), int((w+h)/3), (255, 255, 255), 1)
+    cv2.circle(image, (int(x+w/2), int(y+h/2)), int((w+h)/3), (255, 255, 255), 1)
+  if len(faces) > 0:
+    lastWatched = thisTime
+  timeSinceWatched = thisTime - lastWatched
+  
+  watchSeconds = 1.0
+  if lastOutput != watched:
+    if timeSinceWatched < watchSeconds:
+      print("watched! will not boil")
+      GPIO.output(iopin, watched)
+      lastOutput = watched
+  elif lastOutput != unwatched:
+    if timeSinceWatched >= watchSeconds:
+      print("it's been a while... will boil")
+      GPIO.output(iopin, unwatched)
+      lastOutput = unwatched
+
   # show the frame
   cv2.imshow("Frame", image)
-  key = cv2.waitKey(1) & 0xFF
-
-  # clear the stream in preparation for the next frame
-  rawCapture.truncate(0)
-
-  # if the `q` key was pressed, break from the loop
-  if key == ord("q"):
+  if cv2.waitKey(1) == 27:
       break
         
   
+cv2.destroyAllWindows()
         
 
